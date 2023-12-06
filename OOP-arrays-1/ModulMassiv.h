133e3ce65f7a781baff037cdd196f4c58b8fcffc
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-07 05:01:15 +0900

    Update ModulMassiv.h

--- OOP-arrays-1/ModulMassiv.h
+++ OOP-arrays-1/ModulMassiv.h
@@ -33,31 +33,54 @@ using namespace std;
 // ПРОСТРАНСТВО ИМЕН ДЛЯ МАТРИЦЫ
 namespace NamespaceMassiv
 {
-	//---функция выделение памяти для массива размерности n
-	//int* Memo_Mass(unsigned n);
-
 	//---процедура ввода массива размерности n случайными числами
 	// Динамические массивы в функции передаются как векторы
 	// ничего не возвращает, просто вводит данные в массив
-	//void Vvod_Mass(int* a, unsigned n);
-	void Vvod_Mass(vector<double>& a);
+	template <typename type>
+	void Vvod_Mass(vector<type>& a)
+	{
+		// массив a – формальные параметры
+		// Генерирует случайное число, используя текущею дату, для непредсказуемости результата
+		srand(time(0)); // инициализация генератора случайных чисел текущим временем в секундах time
+		// заполнение массива случайными числами от -100 до 100 включительно
+		for (unsigned i = 0; i < a.size(); ++i) // нумерация с нуля до n-1
+		{
+			a[i] = rand() % 200 - 100;
+			// rand() возвращает случайное целое число от 0 до RAND_MAX
+		}
+	}
+
 	//---процедура вывода на экран массива N
 	// ничего не возвращает, просто выводит на консоль
-	void Screen_Mass(vector<double>& a);
+	template<typename type>
+	void Screen_Mass(vector<type>& a)
+	{
+		// массив a – формальные параметры
+		for (unsigned i = 0; i < a.size(); i++)
+		{
+			cout << "a[" << i << "] = " << a[i] << " ";
+		}
+		cout << "\n";
+	}
 
 	//---функция расчета суммы |a1| + ... + |an|
 	// массив a – формальные параметры
 	// возвращает сумму типа int
-	int Sum_Mass(vector<double>& a);
+	template<typename type>
+	int Sum_Mass(vector<type>& a)
+	{
+		// определение переменной (объявление + инициализация)
+		int sum = 0;
+		for (unsigned i = 0; i < a.size(); ++i)
+		{
+			sum = sum + fabs(a[i]); // fabs - абсолютное значение |a[i]|
+		}
+		return sum; // возврат суммы sum из функции Sum_Mass
+	}
 
 	//---проверка работы функции Sum_Mass на разных входных данных
 	// в саму процедуру нет входящих параметров
 	void Test_Sum_Mass();
-
-	//---функция освобождение памяти массива N
-	// массив *a формальные параметры
-	// ничего не возвращает, просто удаляет память у массива а
-	//void Del_Mass(int* a); 
 }
 
 // ПРОСТРАНСТВО ИМЕН ДЛЯ МАТРИЦЫ В ФАЙЛЕ
@@ -66,16 +89,47 @@ namespace NamespaceFile
 	//---процедурa записи в файл fmass.txt
 	// массив a – формальные параметры
 	// ничего не возвращает, просто пишет массив в файл
-	void WriteFile(vector<double>& a);
+	template<typename type>
+	void WriteFile(vector<type>& a)
+	{
+		// Режим открытия файла для записи ios::out
+		// устанавливается при создании файла
+		ofstream Fin("fmass.txt", ios::out); //затирает старые и пишет новые данные
+		// пишем в файл отправленный массив
+		for (unsigned i = 0; i < a.size(); ++i)
+		{
+			Fin << a[i] << " ";
+		}
+		Fin << endl;
+		Fin.close(); // Функция close() закрывает поток файла
+		cout << "Write. End of recording" << endl;
+	}
 
 	//-----процедурa чтения из файла fmass.txt
 	// n – формальные параметры - размерность массива
 	// возвращает прочитанный массив a из файла
 	vector<double> ReadFile(unsigned n);
-
+	
 	//---процедурa добавления записи в файл fmass.txt
 	// массив a – формальные параметры
 	// ничего не возвращает, просто пишет массив в файл
-	void AddFile(vector<double>& a);
-}
+	template<typename type>
+	void AddFile(vector<type>& a)
+	{
+		//Режим открытия файла для добавления записи ios::app
+		//устанавливается при создании файла
+		ofstream Fin("fmass.txt", ios::app); // добавление в конец файла
+		// пишем в файл отправленный массив
+		for (unsigned i = 0; i < a.size(); ++i)
+		{
+			Fin << a[i] << " ";
+		}
+		Fin << endl;
+		Fin.close(); /// Функция fclose() закрывает поток файла
+		cout << "Add. End of recording" << endl;
+		cout << "\n";
+	}
 
+	//---функция поиска количества элементов массива в файле
+	unsigned SizeFile(const string& file_name);
+}
