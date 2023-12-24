b1e63006e536b9dee25d320c09b3b6ae41f0e57d
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-25 01:49:06 +0900

    Update Locality.h

--- ClassLocality/Locality.h
+++ ClassLocality/Locality.h
@@ -1,34 +1,44 @@
 /// \file Locality.h
-/// \brief Заголовочный файл модуля Locality.cpp
+/// \brief Заголовочный файл модуля Locality.cpp с классом Locality
 /// \brief Класс Населённый пункт
 /// \author Бурдинская Наталья ВМК-22
 /// \date 15.12.2023
 
 #pragma once
-#include <iostream>
-#include <cstring>
+#include <iostream> // ввод-вывод
+#include <string> // для работы со строками
+// теперь можно обращаться ко всем именам, описанным в std, непосредственно, без префикса std::
+using namespace std;
 
 class Locality
 {
 protected:
-	std::string name;
-	std::string population;
-	int home;
+	string name; // имя населенного пункта
+	string power; // власть
+	int population;	// численность населения
 public:
-	Locality(std::string Locality_name = "-", std::string Locality_population = "-", int Locality_home = 0);
+	// перечислимый тип видов (город, поселок, деревня)
+	// До 1000 человек – деревня, до 30 000 человек – посёлок, более 30 000 – город
+	enum LocType { City, City_Village, Village, Other};
+	LocType type; // переменная типа класса LocType
+	Locality (string Locality_name = "-", string Locality_power = "-", int Locality_population = 0);
 
-	void set_name(std::string new_name);
-	std::string get_name();
+	string get_name () const;
+	void set_name (string new_name);
 
-	void set_population(std::string new_population);
-	std::string get_population();
+	string get_power () const;
+	void set_power (string new_power);
 
-	void set_home(int set_home);
-	int get_home();
+	int get_population () const;
+	void set_population (int set_population);
 
-	std::string live();
-	std::string live(int number_of_population);
-	std::string work();
+	string live() const;
+	string live(int number_of_population) const;
+	///string work() const;
+	string walk() const;
+	virtual string work() const;
 
-	virtual std::string walk();
+	// полиморфный вывод полей класса Locality, применим к наследуемым классам
+	virtual void Output() const;
 };
+
