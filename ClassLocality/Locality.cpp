7df816b40f52e6c1836a88dcb9728cbe992aae72
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-25 01:48:09 +0900

    Update Locality.cpp

--- ClassLocality/Locality.cpp
+++ ClassLocality/Locality.cpp
@@ -1,65 +1,129 @@
-/// \file Locality.cpp
-/// \brief cpp-файл модуль Locality.cpp
+/// \file Locality.cpp 
+/// \brief cpp-файл модуль Locality.cpp с классом Locality
 /// \brief Класс Населённый пункт
 /// \author Бурдинская Наталья ВМК-22
 /// \date 15.12.2023
 
-#include "Locality.h"
+#include "Locality.h" // Заголовочный файл с классом Locality
+#include "City.h"
+#include "Village.h"
 
-// Класс 
-Locality::Locality(std::string Locality_name, std::string Locality_population, int Locality_home) {
-	name = Locality_name;
-	population = Locality_population;
-	home = Locality_home; // 2 - частный дом, > 2 - многоквартирный дом
+// Класс Населенный пункт
+Locality::Locality(string Locality_name, string Locality_power, int Locality_population) {
+	name = Locality_name; // имя населенного пункта
+	power = Locality_power; // власть
+	population = Locality_population; // численность населения
 }
 
-void Locality::set_name(std::string new_name) {
+string Locality::get_name() const {
+	return name;
+}
+
+void Locality::set_name(string new_name) {
 	if (new_name != "")
 		name = new_name;
 	else throw std::invalid_argument("Impossible Locality name");
 }
 
-std::string Locality::get_name() {
-	return name;
+string Locality::get_power() const {
+	return power;
 }
 
-void Locality::set_population(std::string new_population) {
-	if (new_population != "")
+void Locality::set_power(string new_power) {
+	if (new_power != "")
+		power = new_power;
+	else throw std::invalid_argument("Impossible Locality name");
+}
+
+int Locality::get_population() const {
+	return population;
+}
+
+void Locality::set_population(int new_population) {
+	if (new_population != 0)
 		population = new_population;
 	else throw std::invalid_argument("Impossible Locality population");
 }
 
-std::string Locality::get_population() {
-	return population;
+string Locality::live() const {
+	return name + ": - population " + to_string (population) + " mans ";
 }
 
-void Locality::set_home(int new_home) {
-	if (new_home >= 2)
-		home = new_home;
-	else throw std::invalid_argument("Impossible Locality home");
-}
-
-int Locality::get_home() {
-	return home;
-}
-
-std::string Locality::live() {
-	return name + ": population ";
-}
-
-std::string Locality::live(int number_of_population) {
-	if (number_of_population < 50000) {
-		std::string s = name + ": - ";
-		s = s + " small_population";
-		return s;
+// До 1000 человек – деревня, до 30 000 человек – посёлок, более 30 000 – город
+string Locality::live (int number_of_population) const {
+	string Okey = "Other"; 
+	if (number_of_population <= 1000)
+	{
+		LocType type = LocType::Village; // деревня    
+		Okey = "Village";
 	}
-	else return name + ": - " + " large_population";
+	if ((number_of_population > 1000) && (number_of_population <= 30000))
+	{
+		LocType type = LocType::City_Village; // поселок городского типа
+		Okey = "City_Village";
+	}
+	if (number_of_population > 30000)
+	{
+		LocType type = LocType::City; // город
+		Okey = "City";
+	}
+	if (Okey == "Other")
+	{
+		LocType type = LocType::Other; // другой
+	}
+	string s = name + ": - ";
+	s = s +  Okey + ", population " + to_string (number_of_population) + " mans, " + " management bodies : " + power;
+	
+	return s;
 }
 
-std::string Locality::work() {
-	return name + " work people";
+string Locality::work () const {
+	string ss = "  Other"; // разбиваем перечислимый тип на строки
+	if (population > 30000)
+		// Люди работают в больницах, школах, на предприятиях и в торговых центрах.
+		ss = ": - City. People work in hospitals, schools, businesses, and shopping malls.";
+
+	if ((population > 1000) && (population <= 30000))
+        // Люди работают в медицинских центрах, школах, магазинах, занимаются сельским хозяйством.
+		ss = ": - City_Village. People work in medical centers, schools, shops, and are engaged in agriculture.";
+
+	if (population <= 1000)
+		// Люди занимаются сельским хозяйством.
+		ss = ": - Village. People are engaged in agriculture.";
+
+	if (ss == "  Other")
+		// Неопределенное место для работы.
+		ss = ": - Other. An undefined place to work.";
+
+	return name + ss;
 }
 
-std::string Locality::walk() {
-	return name + " walk people";
+string Locality::walk() const {
+	string ss = "  Other"; // разбиваем перечислимый тип на строки
+	if (population > 30000)
+		// Люди гуляют в парках, ходят в кино, музеи, рестораны
+		ss = ": - City. People walk in parks, go to the cinema, museums, restaurants.";
+
+	if ((population > 1000) && (population <= 30000))
+		// Люди гуляют по улицам, в лесах, ходят в клубы.
+		ss = ": - City_Village. People walk the streets, in the woods, go to clubs.";
+
+	if (population <= 1000)
+		// Люди гуляют по лесу, купаются в водоемах.
+		ss = ": - Village. People walk in the woods, swim in reservoirs.";
+
+	if (ss == "  Other")
+		// Неопределенное место для прогулок.
+	    ss = ": - Other. An undefined place to walk.";
+
+	return name + ss;
+}
+
+void Locality::Output() const {
+	cout << endl;
+	cout << " Locality  " << endl;
+	cout << " Name       : " << name << endl;
+	cout << " Power      : " << power << endl;
+	cout << " Population : " << population << endl;
+	cout << endl;
 }
