f62ecacae196723066ec8f8f10c46005fadcb226
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-25 01:52:11 +0900

    Update Village.h

--- ClassLocality/Village.h
+++ ClassLocality/Village.h
@@ -1,17 +1,26 @@
 /// \file Village.h
-/// \brief Заголовочный файл модуля Village.cpp
+/// \brief Заголовочный файл модуля Village.cpp с наследником класса Locality
 /// \brief Класс Деревня
 /// \author Бурдинская Наталья ВМК-22
 /// \date 15.12.2023
 
 #pragma once
-#include "Locality.h"
-class Village :
-	public Locality
-{
+#include "Locality.h" // Заголовочный файл с классом Locality
+// класс деревня
+class Village :	public Locality {
+private:
+	string name; // имя населенного пункта
+	int population; // численность населения
+	string TypeLoc; // тип населенного пункта
+	int House; // жилье - дом, 1 - одноэтажные, больше 1 - многоэтажные
 public:
-	Village(std::string Village_name, std::string Village_population, int Village_home);
-	std::string live();
-	std::string work();
-	std::string walk() override;
+	// конструктор класса город
+	Village(string name = "-", int population = 0, string TypeLoc = "Other", int House = 0);
+	string wooden_houses() const;
+	string floors() const;
+
+	string work() const override;
+
+	//вывод полей класса Locality
+	void Output() const override;
 };
