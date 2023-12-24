c8a244f5247a0e649d7284be53edcc844106fb76
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-25 01:49:53 +0900

    Update City.cpp

--- ClassLocality/City.cpp
+++ ClassLocality/City.cpp
@@ -1,20 +1,60 @@
 /// \file City.cpp
-/// \brief cpp-файл модуль City.cpp
+/// \brief cpp-файл модуль City.cpp с наследником класса Locality
 /// \brief Класс Город
 /// \author Бурдинская Наталья ВМК-22
 /// \date 15.12.2023
 
-#include "City.h"
+#include "City.h" // Заголовочный файл с наследником класса Locality
+#include "Locality.h" // Заголовочный файл с классом Locality
 
-City::City(std::string City_name, std::string City_population, int City_home) {
-	name = City_name;
-	population = City_population;
-	home = City_home; // 2 - частный дом, > 2 - многоквартирный дом
+City::City (string City_name, int City_population, string City_TypeLoc, int City_house) {
+	name = City_name; // наименование города
+	population = City_population; // численность населения
+	TypeLoc = City_TypeLoc; // тип населенного пункта
+	House = City_house; // жилье - дом, 1 - одноэтажные, больше 1 - многоэтажные
 }
-std::string City::work() const {
-	return name + " work on zavod";
+string City::stone_houses() const {
+	// дома кирпичные и панельные.
+	return name + " - the houses are brick and panel.";
 }
 
-std::string City::walk() const {
-	return name + " walk in Museum";
+string City::floors() const {
+	// дома и сооружения здесь многоэтажные.
+	return name + " - the houses and buildings are multi-storey.";
+}
+
+string City::work() const {
+	string ss = "  Other"; // разбиваем перечислимый тип на строки
+	if (population > 30000)
+		// Люди работают в больницах, школах, на предприятиях и в торговых центрах.
+		ss = ": - City. People work in hospitals, schools, businesses, and shopping malls.";
+
+	if ((population > 1000) && (population <= 30000))
+		// Люди работают в медицинских центрах, школах, магазинах, занимаются сельским хозяйством.
+		ss = ": - City_Village. People work in medical centers, schools, shops, and are engaged in agriculture.";
+
+	if (population <= 1000)
+		// Люди занимаются сельским хозяйством.
+		ss = ": - Village. People are engaged in agriculture.";
+
+	if (ss == "  Other")
+		// Неопределенное место для работы.
+		ss = ": - Other. An undefined place to work.";
+
+	return name + ss;
+}
+
+void City::Output() const {
+	string s1 = stone_houses();
+	string s2 = floors();
+	string s3 = work();
+	cout << endl;
+	cout << " City         " << endl;
+	cout << " Name       : " << name << endl;
+	cout << " Population : " << population << endl;
+	cout << " TypeLoc    : " << TypeLoc << endl;
+	cout << " House      : " << s1 << endl;
+	cout << " Floors     : " << s2 << endl;
+	cout << " Work       : " << s3 << endl;
+	cout << endl;
 }
