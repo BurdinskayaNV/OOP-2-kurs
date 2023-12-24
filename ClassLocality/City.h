b615b3720a0e7ceac12f17e293e31e6567e52876
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2023-12-25 01:50:46 +0900

    Update City.h

--- ClassLocality/City.h
+++ ClassLocality/City.h
@@ -1,19 +1,27 @@
 /// \file City.h
-/// \brief Заголовочный файл модуля City.cpp
+/// \brief Заголовочный файл модуля City.cpp с наследником класса Locality
 /// \brief Класс Город
 /// \author Бурдинская Наталья ВМК-22
 /// \date 15.12.2023
 
 #pragma once
+#include "Locality.h" // Заголовочный файл с классом Locality
 #include "Locality.h"
-#include <iostream>
-#include <cstring>
-
+// класс город
 class City : public Locality {
 private:
-	//std::string TypeLoc;
+	string name; // имя населенного пункта
+	int population; // численность населения
+	string TypeLoc; // тип населенного пункта
+	int House; // жилье - дом, 1 - одноэтажные, больше 1 - многоэтажные
 public:
-	City(std::string City_name = "-", std::string City_population = "-", int City_home = 0);
-	std::string work() const;
-	std::string walk() const;
+	// конструктор класса город
+	City (string name = "-", int population = 0, string TypeLoc = "Other", int House = 0);
+	string stone_houses () const;
+	string floors () const;
+	
+	string work() const override;
+
+	//вывод полей класса Locality
+	void Output() const override;
 };
