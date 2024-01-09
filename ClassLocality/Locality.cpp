1ddd72963873f05cf3b6e96e7448cd41201d964e
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-01-09 23:27:00 +0900

    Update Locality.cpp

--- ClassLocality/Locality.cpp
+++ ClassLocality/Locality.cpp
@@ -5,125 +5,98 @@
 /// \date 15.12.2023
 
 #include "Locality.h" // Заголовочный файл с классом Locality
-#include "City.h"
-#include "Village.h"
+#include <stdexcept> // используемых для сообщения об исключениях exception
+#include <iostream> // ввод-вывод
+#include <cassert> // для assert
 
 // Класс Населенный пункт
-Locality::Locality(string Locality_name, string Locality_power, int Locality_population) {
-	name = Locality_name; // имя населенного пункта
-	power = Locality_power; // власть
-	population = Locality_population; // численность населения
+// Конструктор с параметрами
+Locality::Locality(string Loc_name, int Loc_population, LocType Loc_type, string Loc_power, string Loc_info) {
+	name = Loc_name; // имя населенного пункта
+	set_population(Loc_population); // численность населения
+	set_type(Loc_type); // тип населенного пункта
+	power = Loc_power; // органы власти
+	info = Loc_info; // особая информация
 }
 
-string Locality::get_name() const {
-	return name;
+// сеттер и геттер для численности населения
+void Locality::set_population(int new_population) {
+	if (new_population != 0)
+		population = new_population;
+	else throw invalid_argument("Недействительный аргумент population");
 }
-
-void Locality::set_name(string new_name) {
-	if (new_name != "")
-		name = new_name;
-	else throw std::invalid_argument("Impossible Locality name");
-}
-
-string Locality::get_power() const {
-	return power;
-}
-
-void Locality::set_power(string new_power) {
-	if (new_power != "")
-		power = new_power;
-	else throw std::invalid_argument("Impossible Locality name");
-}
-
 int Locality::get_population() const {
 	return population;
 }
 
-void Locality::set_population(int new_population) {
-	if (new_population != 0)
-		population = new_population;
-	else throw std::invalid_argument("Impossible Locality population");
+// сеттер и геттер для типа населенного пункта
+void Locality::set_type(LocType new_type) {
+	if ((new_type == 0) || (new_type == 1) || (new_type == 2) || (new_type == 3))
+		type = new_type;
+	else throw invalid_argument("Недействительный аргумент LocType");
+}
+string Locality::get_type() const {
+	if (type == 0)
+	{
+		return "Город";
+	}
+	if (type == 1)
+	{
+		return "Деревня";
+	}
+	return "Другое";
 }
 
+// в населенном пункте живут
 string Locality::live() const {
-	return name + ": - population " + to_string (population) + " mans ";
+	string ss = "Другое";
+	if (type == 0) { ss = "Город"; }
+	if (type == 1) { ss = "Деревня"; }
+	return "В населенном пункте " + name + ": - население " + to_string(population) + " человек, что соответствует типу " + ss + ".";
 }
 
-// До 1000 человек – деревня, до 30 000 человек – посёлок, более 30 000 – город
-string Locality::live (int number_of_population) const {
-	string Okey = "Other"; 
-	if (number_of_population <= 1000)
-	{
-		LocType type = LocType::Village; // деревня    
-		Okey = "Village";
-	}
-	if ((number_of_population > 1000) && (number_of_population <= 30000))
-	{
-		LocType type = LocType::City_Village; // поселок городского типа
-		Okey = "City_Village";
-	}
-	if (number_of_population > 30000)
-	{
-		LocType type = LocType::City; // город
-		Okey = "City";
-	}
-	if (Okey == "Other")
-	{
-		LocType type = LocType::Other; // другой
-	}
-	string s = name + ": - ";
-	s = s +  Okey + ", population " + to_string (number_of_population) + " mans, " + " management bodies : " + power;
-	
-	return s;
-}
-
+// в населенном пункте работают, занимаются
 string Locality::work () const {
-	string ss = "  Other"; // разбиваем перечислимый тип на строки
-	if (population > 30000)
-		// Люди работают в больницах, школах, на предприятиях и в торговых центрах.
-		ss = ": - City. People work in hospitals, schools, businesses, and shopping malls.";
-
-	if ((population > 1000) && (population <= 30000))
-        // Люди работают в медицинских центрах, школах, магазинах, занимаются сельским хозяйством.
-		ss = ": - City_Village. People work in medical centers, schools, shops, and are engaged in agriculture.";
-
-	if (population <= 1000)
-		// Люди занимаются сельским хозяйством.
-		ss = ": - Village. People are engaged in agriculture.";
-
-	if (ss == "  Other")
-		// Неопределенное место для работы.
-		ss = ": - Other. An undefined place to work.";
-
-	return name + ss;
+	return "В населенном пункте " + name + " люди работают согласно их социальному статусу и территориальному положению.";
 }
 
+// в населенном пункте досуг - гуляют, посещают
 string Locality::walk() const {
-	string ss = "  Other"; // разбиваем перечислимый тип на строки
-	if (population > 30000)
-		// Люди гуляют в парках, ходят в кино, музеи, рестораны
-		ss = ": - City. People walk in parks, go to the cinema, museums, restaurants.";
-
-	if ((population > 1000) && (population <= 30000))
-		// Люди гуляют по улицам, в лесах, ходят в клубы.
-		ss = ": - City_Village. People walk the streets, in the woods, go to clubs.";
-
-	if (population <= 1000)
-		// Люди гуляют по лесу, купаются в водоемах.
-		ss = ": - Village. People walk in the woods, swim in reservoirs.";
-
-	if (ss == "  Other")
-		// Неопределенное место для прогулок.
-	    ss = ": - Other. An undefined place to walk.";
-
-	return name + ss;
+	return "В населенном пункте " + name + " люди занимаются досугом согласно его типу и характеристикам.";
 }
 
+// преобразование в строку
+string Locality::Loc_to_string() const {
+	return "Населенный пункт " + name + ": - население " + to_string(get_population()) + " человек, тип - " + get_type() 
+		+ ", орган власти - " + power + ", дополнительная информация - " + info + ".";
+}
+
+// вывод полей класса Locality
 void Locality::Output() const {
-	cout << endl;
-	cout << " Locality  " << endl;
-	cout << " Name       : " << name << endl;
-	cout << " Power      : " << power << endl;
-	cout << " Population : " << population << endl;
-	cout << endl;
+	string ss = "Другое";
+	if (type == 0)	{ ss = "Город"; }
+	if (type == 1)	{ ss = "Деревня"; }
+	cout << "----------------------------" << endl;
+	cout << " НАСЕЛЕННЫЙ ПУНКТ __________  " << endl;
+	cout << " Наименование              :  " << name << endl;
+	cout << " Численность населения     :  " << population << " человек" << endl;
+	cout << " Тип населенного пункта    :  " << ss << endl;
+	cout << " Орган власти              :  " << power << endl;
+	cout << " Дополнительная информация :  " << info << endl;
+	cout << "----------------------------" << endl;
+}
+
+// проверка работы класса Locality
+void test_Locality() {
+	cout << "Тестирование класса Locality:" << endl;
+	Locality Test1("Чита", 500, Деревня, "Органы местного самоуправления", "Особенности населенных пунктов зависят от их размера и типа");
+	assert(Test1.get_population() == 500);
+	assert(Test1.get_type() == "Деревня");
+
+	Test1.set_population(333679);
+	Test1.set_type(Город);
+	cout << Test1.Loc_to_string() << endl;
+	assert(Test1.Loc_to_string() == "Населенный пункт Чита: - население 333679 человек, тип - Город, орган власти - Органы местного самоуправления, дополнительная информация - Особенности населенных пунктов зависят от их размера и типа.");
+
+	cout << "Тестирование класса 'Locality' выполнено УСПЕШНО" << endl;
 }
