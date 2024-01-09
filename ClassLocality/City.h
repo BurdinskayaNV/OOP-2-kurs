ba89da36df4e2a93ad6cfe656d4965cab081df3e
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-01-09 23:27:48 +0900

    Update City.h

--- ClassLocality/City.h
+++ ClassLocality/City.h
@@ -6,22 +6,44 @@
 
 #pragma once
 #include "Locality.h" // Заголовочный файл с классом Locality
-#include "Locality.h"
-// класс город
-class City : public Locality {
-private:
-	string name; // имя населенного пункта
-	int population; // численность населения
-	string TypeLoc; // тип населенного пункта
-	int House; // жилье - дом, 1 - одноэтажные, больше 1 - многоэтажные
-public:
-	// конструктор класса город
-	City (string name = "-", int population = 0, string TypeLoc = "Other", int House = 0);
-	string stone_houses () const;
-	string floors () const;
-	
-	string work() const override;
+#include <string> // для работы со строками
 
-	//вывод полей класса Locality
+#ifndef LOCCITY_H
+#define LOCCITY_H
+
+// класс LocCity
+class LocCity : public Locality {
+public:
+	// поля наследники
+	int house; // жилье - этажность дома, 1 - одноэтажные, больше 1 - многоэтажные
+	string occupation; // занятия населения
+	string leisure; // досуг населения
+	string Infrastructure; // Инфраструктура и коммуникации
+
+	// Конструктор с параметрами
+	LocCity(string Loc_name = "Locality", int Loc_population = 0, LocType Loc_type = Другое, string Loc_power = "-",
+		    string Loc_info = "-", int Loc_house = 0, string Loc_occupation = "-", string Loc_leisure = "-", string Loc_Infrastructure = "-");
+
+	// люди живут в каких домах
+	string live() const override;
+
+	// люди где работают
+	string work() const override;
+	
+	// люди где проводят досуг
+	string walk() const override;
+
+	// инфраструктура и коммуникации
+	string infr() const;
+
+	// преобразование в строку
+	string Loc_to_string() const override;
+
+	//вывод полей класса LocCity
 	void Output() const override;
 };
+
+// Проверка работы класса LocCity
+void test_City();
+
+#endif
