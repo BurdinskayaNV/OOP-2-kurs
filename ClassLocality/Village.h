398a8d639a3118a0d19ba13edbebf1d2224da85f
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-01-09 23:31:06 +0900

    Update Village.h

--- ClassLocality/Village.h
+++ ClassLocality/Village.h
@@ -6,21 +6,57 @@
 
 #pragma once
 #include "Locality.h" // Заголовочный файл с классом Locality
-// класс деревня
-class Village :	public Locality {
-private:
-	string name; // имя населенного пункта
-	int population; // численность населения
-	string TypeLoc; // тип населенного пункта
-	int House; // жилье - дом, 1 - одноэтажные, больше 1 - многоэтажные
-public:
-	// конструктор класса город
-	Village(string name = "-", int population = 0, string TypeLoc = "Other", int House = 0);
-	string wooden_houses() const;
-	string floors() const;
+#include <string> // для работы со строками
 
+#ifndef LOCVILLAGE_H
+#define LOCVILLAGE_H
+
+// класс LocVillage
+class LocVillage :	public Locality {
+private:
+	// поля наследники
+	int house; // жилье - этажность дома, 1 - одноэтажные, больше 1 - многоэтажные
+	string occupation; // занятия населения
+	string leisure; // досуг населения
+public:
+	string ecology; // экология
+
+	// конструктор класса город
+	LocVillage(string Loc_name = "Locality", int Loc_population = 0, LocType type = Другое, string Loc_power = "-", string Loc_info = "-",
+		int Loc_house = 0, string Loc_occupation = "-", string Loc_leisure = "-", string ecology = "-");
+		
+	// сеттер и геттер для этажности дома
+	void set_house(int new_house);
+	int get_house() const;
+
+	// сеттер и геттер для занятости населения
+	void set_occupation(string new_occupation);
+	string get_occupation() const;
+
+	// сеттер и геттер для досуга населения
+	void set_leisure(string new_leisure);
+	string get_leisure() const;
+
+	// люди живут в каких домах
+	string live() const override;
+
+	// люди где работают
 	string work() const override;
 
-	//вывод полей класса Locality
+	// люди где проводят досуг
+	string walk() const override;
+
+	// экология
+	string eco() const;
+
+	// преобразование в строку
+	string Loc_to_string() const override;
+
+	//вывод полей класса LocVillage
 	void Output() const override;
 };
+
+// Проверка работы класса LocVillage
+void test_Village();
+
+#endif
