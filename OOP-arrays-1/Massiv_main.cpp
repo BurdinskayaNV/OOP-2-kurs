7738c4476ff94dc3379dda8752feb26f8577162a
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-01-12 01:15:28 +0900

    Update Massiv_main.cpp

--- OOP-arrays-1/Massiv_main.cpp
+++ OOP-arrays-1/Massiv_main.cpp
@@ -20,86 +20,121 @@
 *  действительные числа a1,..., an.
 *  Вычислить: в) |a1| + ... + |an| */
 
+//Объявляет cin cout,управляющие чтением из станд.потоков и записью в них
+#include <iostream>
 // вызов заголовочного файла модуля ModulMassiv.cpp
 #include "ModulMassiv.h"
 // пространство имён для работы с матрицей
 using namespace NamespaceMassiv;
 // пространство имён для работы с матрицей в файлах
 using namespace NamespaceFile;
+//теперь можно обращаться ко всем именам, описанным в std, непосредственно, без префикса std::
+using namespace std;
 
 int main(int argc, char* argv[]) //главная функция, вызываемая при запуске программы
 {
 	// Функция setlocale задаёт локализацию программы. 
 	// LC_ALL указывает программе, что локализированы будут все функции.
 	// «Rus» локализация произойдёт на русский язык.
+	system("chcp 65001 > nul"); //подключение русского языка 
 	setlocale(LC_ALL, "RUSSIAN");
 	// Аргументы командной строки cmd arg для функции main
 	// argv[0] - имя файла Massiv_main.exe
 	// значит начинаем argv[1] и argv[2] 
 	// в нашем случае argc = 3
 	// (argv[0] имя самого проекта, argv[1] размерность массива и argv[2] имя файла для ввода и сохранения данных)
-	if (argc == 3) {
-		int n = stoi(argv[1]); // преобразовать строку в int 
-		string file_name = argv[2];
-	}
-	else { //блок try-catch
-		cout << "Number of array elements" << endl;
+	string str_argv;
+	string file_name;
+	int n = 0;
+	int k = argc;
+	if (k == 1)
+	{
 		try {
-			int n = ReadArrLength(); // отсутствует argv[1] или не переведется из стоки в int
+			cout << "Введите размерность массива" << endl;
+			cout << "n = "; cin >> n;
 		}
-		catch (const std::length_error err) {   
+		catch (const length_error err) {
 			cout << err.what() << endl; // выводим сообщение об ошибке
 		}
-		cout << "Input filename:" << endl;
 		try {
-			string file_name = ReadFileName(); // отсутствует argv[2] или некорректный ввод имени файла
+			cout << "Введите имя файла " << endl;
+			cout << "file_name = "; cin >> file_name;
+		}
+		catch (const invalid_argument inval) {   //ловим исключение
+			cout << inval.what() << endl;
+		}
+		cout << "\n";
+	}
+	
+	if (k == 2)
+	{
+		str_argv = argv[1];
+		try {
+			if (str_argv == "help")	// Если введут help
+			{
+				cout << "Чтобы программа работала корректно, введите первый аргумент число: <размерность массива>" << endl;
+				cout << "Далее <имя файла для ввода и сохранения данных>" << endl;
+				return 0;
+			}
+		}
+		catch (const invalid_argument inval) {   //ловим исключение
+			cout << inval.what() << endl;
+		}
+		cout << "\n";
+	}
+
+	if (k == 3)
+	{
+		try {
+			n = stoi(argv[1]); // преобразовать строку в int 
+		}
+		catch (const std::length_error err) {
+			cout << err.what() << endl; // выводим сообщение об ошибке
+		}
+		try {
+			file_name = argv[2];
 		}
 		catch (const std::invalid_argument inval) {//ловим исключение
 			cout << inval.what() << endl;
 		}
 	}
 
-	int n = stoi(argv[1]); // преобразовать строку в int 
-	vector<double> arr; // описываем массив как вектор
-	arr.resize(n); // даем ему размерность n из переданного аргумента argv[1]
+		vector<double> arr; // описываем массив как вектор
+		arr.resize(n); // даем ему размерность n из переданного аргумента argv[1]
 
-	// Задаем размерность массива
-	//cout << "Dimension of the massiv N " << endl;
-	//cout << "n = "; cin >> n;
-	
-	Vvod_Mass(arr); // выполняем ввод массива
-	Screen_Mass(arr); // выводим на экран массив введеных коэффициентов
-	cout << "\n";
+		Vvod_Mass(arr); // выполняем ввод массива
+		Screen_Mass(arr); // выводим на экран массив введеных коэффициентов
+		cout << "\n";
 
-	// проверка работы функции Sum_Mass на разных входных данных
-	Test_Sum_Mass();
-	cout << "Summa |a1| + ... + |an| = " << Sum_Mass(arr) << endl;
-	cout << "\n";
+		// проверка работы функции Sum_Mass на разных входных данных
+		Test_Sum_Mass();
+		cout << "Сумма |a1| + ... + |an| = " << Sum_Mass(arr) << endl;
+		cout << "\n";
 
-	// выполняем запись массива в файл
-	WriteFile(arr);
-	cout << "\n";
-	// читаем из файла массив и выдаем на консоль
-	string file_name = argv[2]; // название файла
-	try   // проверка на исключения
-	{
-		// Заполняем массив arr1 из файла fmass.txt
-		// это защищенный блок кода 
-		// при чтении массива может возникнуть исключение 
-		// Находим количество элементов массива из файла
-		unsigned size_1 = SizeFile(file_name);
-	}
-	catch (const exception& error) // файл не открыт или пуст
-	{	
-		// Код, который выполняется при возникновении исключения типа
-		// Exception генерируется в блоке try
-		// Зарегистрировать сообщение об ошибке в объекте exception
-		cout << error.what() << endl;
-	}
-	vector<double> arr1;
-	arr1.resize(n); // Задаем размер size
-	arr1 = ReadFile(n);
-	Screen_Mass(arr1); // выводим на экран массив введеных коэффициентов
-	arr.clear();
+		// выполняем запись массива в файл
+		WriteFile(file_name, arr);
+		cout << "\n";
+		// читаем из файла массив и выдаем на консоль
+		// file_name = argv[2]; // название файла
+		try   // проверка на исключения
+		{
+			// Заполняем массив arr1 из файла fmass.txt
+			// это защищенный блок кода 
+			// при чтении массива может возникнуть исключение 
+			// Находим количество элементов массива из файла
+			unsigned size_1 = SizeFile(file_name);
+		}
+		catch (const exception& error) // файл не открыт или пуст
+		{
+			// Код, который выполняется при возникновении исключения типа
+			// Exception генерируется в блоке try
+			// Зарегистрировать сообщение об ошибке в объекте exception
+			cout << error.what() << endl;
+		}
+		vector<double> arr1;
+		arr1.resize(n); // Задаем размер size
+		arr1 = ReadFile(file_name, n);
+		Screen_Mass(arr1); // выводим на экран массив введеных коэффициентов
+		arr.clear();
 	return 0;
 }
